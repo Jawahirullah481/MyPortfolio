f1b382ee3e4837e412a4ca01c1191d8ba737265b
Jawahirullah481 <112153331+Jawahirullah481@users.noreply.github.com>
2023-06-11 17:36:46 +0530

    Update general.js

--- js/general.js
+++ js/general.js
@@ -3,6 +3,7 @@ var header, projectmenu, iconCloseProjects;
 window.addEventListener("load", function(){
     header = document.getElementById("header");
     projectmenu = document.getElementById("sidebar"); 
+    iconCloseProjects = document.getElementById("icon-close-projectview");
     
     if(document.title != "My Projects")
     document.getElementById("load-wrapper").style.visibility = "hidden";
@@ -22,9 +23,11 @@ function closeMenu()
 function showProjects()
 {
     projectmenu.style.visibility = "visible";
+    iconCloseProjects.style.visibility = "visible";
 }
 
 function closeProjects()
 {
     projectmenu.style.visibility = "hidden";
+    iconCloseProjects.style.visibility = "hidden";
 }
